8de87508b2eebd0b29c0cea587fa7842fd779297
Krish Sridar <krishkhavin@gmail.com>
2025-01-28 10:39:38 -0800

    basic recursive solution

--- interview.py
+++ interview.py
@@ -1,9 +1,12 @@
 ### Holiday Gift Exchange
 # Input: list of people, list of partners / relationships
 # A, B, C
-# Output: [A, B], [B, C], [C, A]
+# Output: {"A" : "B", [B, C], [C, A]}
 
 # Constraint: Partners cannot give gifts to each other
+
+import random
+
 
 class GiftExchange:
 
@@ -17,10 +20,26 @@ class GiftExchange:
         self.relationships[name] = partner
         self.relationships[partner] = name
     
-    
+    def draw_names(self):
+        output = {}
+        available_participants = self.participants.copy()
+        random.shuffle(available_participants)
 
-
-
+        for p in self.participants:
+            resulting_partner = None
+            for a in available_participants:
+                  if a != p and self.relationships[p] != a:
+                      resulting_partner = a
+                      available_participants.remove(a)
+            
+            if resulting_partner:
+                output[p] = resulting_partner
+            else:
+                 print("No match found")
+                 self.draw_names()
+        
+        return output
+            
 
 def testBasic():
     game = GiftExchange()
@@ -28,10 +47,17 @@ def testBasic():
     print(game.participants)
     print(game.relationships)
 
+def testDraw():
+    game = GiftExchange()
+    game.add_participant("A", "B")
+    game.add_participant("C", "D")
+    output = game.draw_names()
+    print(output)
 
 
 def main():
-    testBasic()
+    testDraw()
+
 
 
 
